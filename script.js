73be4c0ba9486b559bef756dce141eee8f441bfb
Suman Kisku <sumankisku1@gmail.com>
2023-01-06 01:36:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your code here
+//your code here
+const count = document.getElementById("letterCount");
+const input = document.getElementById("evaluatedText");
+
+input.addEventListener("input", ()=> {
+    updateCount(input.value.length)
+});
+
+function updateCount(num) {
+    count.innerText = num;
+}
